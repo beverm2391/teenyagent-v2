9306698accaccc47375a568d27ac1fb7ef04cdb7
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2025-01-13 12:06:18 -0800

    checkpoint

--- tests/unit/test_memgpt.py
+++ tests/unit/test_memgpt.py
@@ -3,9 +3,18 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_memgpt():
+async def test_update_memory():
     memgpt = MemGPT()
     await memgpt.chat("My name is ben and i really like potatoes also the sky is blue. also you need to remember my name please")
 
     assert len(memgpt.working_memory) > 0, "nothing got stored in working memory"
     assert len(memgpt.archive) > 0, "nothing got stored in archive"
+
+async def test_trim_conversation():
+    pass
+
+async def test_trim_working_memory():
+    pass
+
+async def test_handle_memory_pressure():
+    pass
